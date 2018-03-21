c8e9df29d0bc4d8e225c0adbba7547b8b997e212
yowchun <yowchun93@gmail.com>
2018-03-21 10:21:10 +0800

    added react router components

---
+++ catch-of-the-day/src/components/StorePicker.js
@@ -0,0 +1,17 @@
+import React from 'react'
+// importing helper functions
+import { getFunName } from '../helpers';
+
+class StorePicker extends React.Component {
+  render() {
+    return (
+      <form action="" className="store-selector">
+        <h2>Please Enter A Store </h2>
+        <input type="text" required placeholder="Store Name"/>
+        <button type="submit"> Visit Store </button>
+      </form>
+    )
+  }
+}
+
+export default StorePicker;
--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,1 +1,10 @@
+// import Component from React
+import React from 'react';
+import { render } from 'react-dom';
+import StorePicker from './components/StorePicker';
+import App from './components/App';
+import Router from "./components/Router";
+import './css/style.css';
+
 // let's go!
+render(<Router />, document.querySelector('#main'));
