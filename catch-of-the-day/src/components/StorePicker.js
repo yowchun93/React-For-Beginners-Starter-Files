b25981fb55d2f95ddb03c71a7bf7ea44e428d1c2
yowchun <yowchun93@gmail.com>
2018-03-21 10:28:08 +0800

    added default value to form

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -2,12 +2,13 @@ import React from 'react'
 // importing helper functions
 import { getFunName } from '../helpers';
 
+
 class StorePicker extends React.Component {
   render() {
     return (
       <form action="" className="store-selector">
         <h2>Please Enter A Store </h2>
-        <input type="text" required placeholder="Store Name"/>
+        <input type="text" required placeholder="Store Name" defaultValue={getFunName()} />
         <button type="submit"> Visit Store </button>
       </form>
     )
